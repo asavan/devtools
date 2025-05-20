2d345426e80745f34c5ffeb41673382d209a3439
a.savanovich <asavanster@gmail.com>
2025-05-20 17:09:56 +0200

    devtools

--- src/totp.js
+++ src/totp.js
@@ -2,6 +2,8 @@ import * as OTPAuth from "otpauth";
 
 import { TOTP } from "totp-generator";
 
+const secret_word = process.env.USER_KEY;
+
 {
     const totp = new OTPAuth.TOTP({
         // Provider or service the account is associated with.
@@ -16,7 +18,7 @@ import { TOTP } from "totp-generator";
         period: 30,
         // Arbitrary key encoded in base32 or OTPAuth.Secret instance
         // (if omitted, a cryptographically secure random secret is generated).
-        secret: process.env.USER_KEY, // or `OTPAuth.Secret.fromBase32("NB2W45DFOIZA")` or `new OTPAuth.Secret()`
+        secret: secret_word, // or `OTPAuth.Secret.fromBase32("NB2W45DFOIZA")` or `new OTPAuth.Secret()`
     });
 
     const token = totp.generate();
@@ -26,6 +28,6 @@ import { TOTP } from "totp-generator";
 }
 
 {
-    const { otp, expires } = TOTP.generate(process.env.USER_KEY);
-    console.log(otp, expires);
+    const { otp, expires } = TOTP.generate(secret_word);
+    console.log(otp, new Date(expires));
 }
